2dc6485d193ea7a30a1d6c16b9fe7176d1d8e9ef
Andre Moeller <13366062+andregmoeller@users.noreply.github.com>
2018-12-04 01:53:51 +0100

    Adds Users

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const storeController = require('../controllers/storeController');
+const userController = require('../controllers/userController');
+const authController = require('../controllers/authController');
 const { catchErrors } = require('../handlers/errorHandlers');
 
 router.get('/', catchErrors(storeController.getStores));
@@ -18,5 +20,13 @@ router.get('/stores/:id/edit', catchErrors(storeController.editStore));
 router.get('/store/:slug', catchErrors(storeController.getStoreBySlug));
 router.get('/tags', catchErrors(storeController.getStoresByTag));
 router.get('/tags/:tag', catchErrors(storeController.getStoresByTag));
+router.get('/login', userController.loginForm);
+router.get('/register', userController.registerForm);
+router.post('/register',
+    userController.validateRegister,
+    userController.register,
+    authController.login);
+router.get('/logout', authController.logout);
+router.post('/login', authController.login);
 
 module.exports = router;
